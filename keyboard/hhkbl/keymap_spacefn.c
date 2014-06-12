76757586e261531517b629dd28c6a1761a5d6d46
Andrey Paskal <apaskal@gmail.com>
2014-06-12 13:58:42 +0400

    Fn layer mapped to Fn button

--- keyboard/hhkbl/keymap_spacefn.c
+++ keyboard/hhkbl/keymap_spacefn.c
@@ -1,22 +1,21 @@
 #include "keymap_common.h"
 
-/*
- * SpaceFN
- * http://geekhack.org/index.php?topic=51069.0
- */
+/*HHKB Lite 2 keyboard*/
+
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Keymap 0: Default Layer
+    /* Layer 0: Default Layer
      * ,-----------------------------------------------------------.
-     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
+     * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|
      * |-----------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|
      * |-----------------------------------------------------------|
-     * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |
+     * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
      * |-----------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
-     * |-----------------------------------------------------------|
-     * |Ctrl|Gui |Alt |      Space             |Alt |Gui |App |Ctrl|
-     * `-----------------------------------------------------------'
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn0|
+     * `-----------------------------------------------------------'-.
+     *     |Fn0|Gui|Alt|         Space         |Alt  |Gui|      | Up |
+     *     `---------------------------------------------' |Left|Down|Right|
+     *                                                     `---------------'
      */
 
     KEYMAP(
@@ -30,26 +29,20 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         SPC, RGUI,                      B,   N,           SLSH           
 	),
 
-	
-    /*KEYMAP_ANSI(*/
-        /*ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \*/
-        /*TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \*/
-        /*CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \*/
-        /*LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT, \*/
-        /*LCTL,LGUI,LALT,          FN0,                     RALT,RGUI,APP, RCTL),*/
 
-    /* Overlay 1: SpaceFN
+    /* Overlay 1: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
-     * |`  | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete |
+     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  \|  `|
      * |-----------------------------------------------------------|
-     * |     |   |   |   |   |   |   |Hom|Up |End|Psc|Slk|Pau|Ins  |
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|
      * |-----------------------------------------------------------|
-     * |      |   |   |   |   |   |PgU|Lef|Dow|Rig|   |   |        |
+     * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
      * |-----------------------------------------------------------|
-     * |        |   |   |   |   |Spc|PgD|`  |~  |   |   |          |
-     * |-----------------------------------------------------------|
-     * |    |    |    |                        |    |    |    |    |
-     * `-----------------------------------------------------------'
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn0|
+     * `-----------------------------------------------------------'-.
+     *     |Fn0|Gui|Alt|         Space         |Alt  |Gui|      | Up |
+     *     `---------------------------------------------' |Left|Down|Right|
+     *                                                     `---------------'
      */
 
     KEYMAP(
@@ -77,6 +70,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Fn action definition
  */
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_TAP_KEY(1, KC_SPACE),
-    [1] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),    // tilde
+    [0] = ACTION_LAYER_MOMENTARY(1),
+    /*[0] = ACTION_LAYER_TAP_KEY(1, KC_SPACE),*/
+    /*[1] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),    // tilde*/
 };
