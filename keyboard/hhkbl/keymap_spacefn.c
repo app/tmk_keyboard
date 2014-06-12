8b2c5a2b31274195f69800347e3be8fe5d0960b0
Andrey Paskal <apaskal@gmail.com>
2014-06-12 21:00:26 +0400

    Fn layer improved

--- keyboard/hhkbl/keymap_spacefn.c
+++ keyboard/hhkbl/keymap_spacefn.c
@@ -32,27 +32,27 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Overlay 1: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
-     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  \|  `|
+     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
      * |-----------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|
+     * |Tab  |  Q|  W|  E|  R|  T|Psc|Pgu| Up|  O|  P|  [|  ]| Del |
      * |-----------------------------------------------------------|
-     * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
+     * |Contro|  A|  S|  D|  F|  G|Hom|Lef|Dow|Rig|End|  '|Enter   |
      * |-----------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn0|
+     * |Shift   |  Z|  X|  C|  V|  B|Del|Pgd|  ,|Fn1|  /|Shift |Fn0|
      * `-----------------------------------------------------------'-.
-     *     |Fn0|Gui|Alt|         Space         |Alt  |Gui|      | Up |
-     *     `---------------------------------------------' |Left|Down|Right|
+     *     |Fn0|Gui|Alt|         Space         |Alt  |Gui|      |PgUp|
+     *     `---------------------------------------------' |Home|PgDn| End |
      *                                                     `---------------'
      */
 
     KEYMAP(
-             LCTL, FN0, GRV,           F5,  F6, F12,      F11,    UP,
-	                  F1, F2, F3,  F4,  F7,  F8,  F9, F10,  DOWN,
-		          Q,  W,   E,   R,PGUP,  UP,   O,   P,  RGHT,
-        BSPC,     LSFT, TAB,            T,   Y,RBRC,LEFT,LBRC,  RALT,
-        BSLS,             A,  S,   D,   F,LEFT,DOWN,RGHT, END,  LALT,
+             LCTL, FN0, DEL,           F5,  F6, F12,      F11,  PGUP,
+	                  F1, F2, F3,  F4,  F7,  F8,  F9, F10,  PGDN,
+		          Q,  W,   E,   R,PGUP,  UP,   O,   P,   END,
+         DEL,     LSFT, TAB,            T,PSCR,RBRC,HOME,LBRC,  RALT,
+         INS,             A,  S,   D,   F,LEFT,DOWN,RGHT, END,  LALT,
              LGUI,      ESC,            G,HOME,          QUOT,       
-	ENT,      RSFT,   Z,  X,   C,   V,PGDN,      DOT,       COMM,
+	ENT,      RSFT,   Z,  X,   C,   V,PGDN,      FN1,       COMM,
         SPC, RGUI,                      B, DEL,           SLSH           
 	
 	),
@@ -71,6 +71,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),
+    [1] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),    // tilde
     /*[0] = ACTION_LAYER_TAP_KEY(1, KC_SPACE),*/
     /*[1] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),    // tilde*/
 };
