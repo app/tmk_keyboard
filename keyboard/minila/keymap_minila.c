9c88da0323d148b88b773984ab6b4d252a098f04
Andrey Paskal <apaskal@gmail.com>
2014-09-13 18:15:31 +0400

    Changes in FN layer for Russian layout

--- keyboard/minila/keymap_minila.c
+++ keyboard/minila/keymap_minila.c
@@ -21,16 +21,16 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // navi and num pad
     KEYMAP(
 	             LSFT, RSFT,  PGUP,      HOME,              \
-	        Q,    TAB,   P0,  INS,    Z,  END,  GRV,    F1, \
+	        Q,    TAB,   P0,  INS,    Z,  END,  BSLS,    F1, \
 	       P7,   LCTL,   P4,         P1,                F2, \
                P8,   LGUI,   P5,         P2,  APP,          F3, \
 	       P9,   NLCK,   P6,    G,   P3,    B,    5,    F4, \
 	     PGUP,   PSCR, LEFT, HOME, PGDN,    N,    6,    F7, \
-	       UP,   RBRC, DOWN,  SPC,  FN2, PGDN,  F12,    F8, \
+	       UP,    GRV, DOWN,  SPC,  FN2, PGDN,  F12,    F8, \
                 O,         RGHT,        NUBS,       CAPS,    F9, \
-                P,   FN1,  END, QUOT,         FN1,  F11,    F10, \
-              FN0,          FN0, LALT,       RALT,             \
-                     BSLS,  DEL,        ENT,        DEL
+                P,   FN1,   END, QUOT,         FN1,  F11,    F10, \
+             TRNS,         TRNS, LALT,       RALT,             \
+                     GRV,  DEL,        ENT,        DEL
     ),
 
 };
