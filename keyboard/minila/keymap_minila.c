e45941456f65898cb870638d3a16bac07d721f33
Andrey Paskal <apaskal@gmail.com>
2015-06-22 19:50:46 +0300

    Full KeyPad on Left side of keyboard.

--- keyboard/minila/keymap_minila.c
+++ keyboard/minila/keymap_minila.c
@@ -21,11 +21,11 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // navi and num pad
     KEYMAP(
 	             LSFT, RSFT,  FN9,      HOME,              \
-	        Q,    TAB,   P0,  INS,    Z,  END,  BSLS,   F1, \
-	       P7,   LCTL,   P4,         P1,                F2, \
+       PSLS,   PENT,   P0,  INS, PDOT,  END,  BSLS,   F1, \
+	       P7,   LCTL,   P4,   P1,                F2, \
                P8,   LGUI,   P5,         P2,  APP,          F3, \
-	       P9,   NLCK,   P6,    G,   P3,    B,   F5,    F4, \
-	     PGUP,   PSCR, LEFT, HOME, PGDN,    N,   F6,    F7, \
+	       P9,   PAST,   P6, PMNS,   P3, PPLS,   F5,    F4, \
+	     PGUP,   PSCR, LEFT, HOME, PGDN, NLCK,   F6,    F7, \
 	       UP,    GRV, DOWN,  SPC,  FN2, PGDN,  F12,    F8, \
                 O,         RGHT,       NUBS,       TRNS,    F9, \
                 P,   FN1,   END, QUOT,         FN1, F11,   F10, \
